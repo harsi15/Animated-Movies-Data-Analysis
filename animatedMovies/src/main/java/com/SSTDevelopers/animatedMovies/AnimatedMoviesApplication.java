149ebae0b5355507b0c714c69f3b2c3a1854005b
Simmy <dhillonharsimran192@gmail.com>
2024-04-13 20:46:56 -0400

    Adding spring initializer code base

---
+++ animatedMovies/src/main/java/com/SSTDevelopers/animatedMovies/AnimatedMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.SSTDevelopers.animatedMovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimatedMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimatedMoviesApplication.class, args);
+	}
+
+}
